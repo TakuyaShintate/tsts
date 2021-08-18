5db653f603fc5175751120f9f9e128bc58d70e24
新立拓也 <kmdbn2hs@gmail.com>
2021-08-19 02:58:40 +0900

    🍎 Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-import os
+from typing import Any, List
 
 from pkg_resources import DistributionNotFound, get_distribution
 from setuptools import find_packages, setup
 
-CHOOSE_INSTALL_REQUIRES = []
 
-
-def choose_requirement(main, secondary):
+def choose_requirement(main: Any, secondary: Any) -> Any:
     try:
         get_distribution(main)
     except DistributionNotFound:
@@ -14,24 +12,21 @@ def choose_requirement(main, secondary):
     return main
 
 
-def get_install_requirements(install_requires, choose_install_requires):
+def get_install_requirements(
+    install_requires: List[str],
+    choose_install_requires: List[Any],
+) -> List[Any]:
     for main, secondary in choose_install_requires:
         install_requires.append(choose_requirement(main, secondary))
     return install_requires
 
 
-def install_requires():
+def install_requires() -> List[str]:
     requires = [
         "numpy",
         "torch",
         "tqdm",
     ]
-    requires += models_requires()
-    return requires
-
-
-def models_requires():
-    requires = ["scikit-image"]
     return requires
 
 
@@ -39,7 +34,8 @@ setup(
     name="tsts",
     packages=[p for p in find_packages() if p.startswith("tsts")],
     install_requires=get_install_requirements(
-        install_requires(), CHOOSE_INSTALL_REQUIRES
+        install_requires(),
+        [],
     ),
     version="0.0.1",
     include_package_data=True,
