d68f7c52f63c40602eab7f723331b53de1425869
Takuya Shintate <kmdbn2hs@gmail.com>
2021-10-24 14:54:36 +0900

    🔧 Fix op order (#75)
    * 🔧 Fix CosineAnnealingWarmRestarts scheduler
    
    * 🔧 Fix op order
    
    * 🍎 Update pad order
    
    * 🍎 Update version

--- tsts/models/scinet.py
+++ tsts/models/scinet.py
@@ -37,7 +37,7 @@ class ConvModule(Module):
 
     def _init_conv_module(self) -> None:
         self.conv_module = Sequential(
-            ReplicationPad1d(2 * (self.kernel_size // 2)),
+            ReplicationPad1d(self.kernel_size // 2),
             Conv1d(
                 self.num_in_feats,
                 int(self.expansion_rate * self.num_in_feats),
@@ -45,6 +45,7 @@ class ConvModule(Module):
             ),
             LeakyReLU(),
             Dropout(self.dropout_rate),
+            ReplicationPad1d(self.kernel_size // 2),
             Conv1d(
                 int(self.expansion_rate * self.num_in_feats),
                 self.num_in_feats,
@@ -112,10 +113,15 @@ class SCIBlock(Module):
         return (f_even, f_odd)
 
     def _scale_and_shift(self, f_even: Tensor, f_odd: Tensor) -> Tuple[Tensor, Tensor]:
-        f_even = f_even * self.psi(f_odd).exp()
-        f_odd = f_odd * self.phi(f_even).exp()
-        f_even = f_even - self.eta(f_odd)
-        f_odd = f_odd + self.rho(f_even)
+        # Escape f_even and f_odd
+        _f_even = f_even
+        _f_odd = f_odd
+        f_even = f_even * self.psi(_f_odd).exp()
+        f_odd = f_odd * self.phi(_f_even).exp()
+        _f_even = f_even
+        _f_odd = f_odd
+        f_even = f_even - self.eta(_f_odd)
+        f_odd = f_odd + self.rho(_f_even)
         return (f_even, f_odd)
 
     def forward(self, mb_feats: Tensor) -> Tuple[Tensor, Tensor]:
