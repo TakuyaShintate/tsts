5918f630614e9621ca04837ed24ac119ff92f86e
新立拓也 <kmdbn2hs@gmail.com>
2022-03-31 19:43:11 +0900

    🍎 Rebuilt docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -5,22 +5,21 @@ import sys
 sys.path.insert(0, os.path.abspath("../tsts"))
 
 project = "tsts"
-copyright = "2021, tsts Development Team"
-author = "tsts Development Team"
+copyright = "2021, tsts development team"
+author = "tsts development team"
 extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
     "sphinx.ext.napoleon",
     "sphinxemoji.sphinxemoji",
     "sphinx.ext.viewcode",
-    # "sphinx_copybutton",
+    "sphinx_copybutton",
     "sphinx.ext.autodoc",
     "sphinx.ext.githubpages",
 ]
 language = "en"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 templates_path = ["_templates"]
-html_theme = "insegel"  # "sizzle"
+html_theme = "karma_sphinx_theme"  # "sizzle"
 # html_static_path = ["_static"]
-# html_logo = "../../img/tsts-logo-small.png"
 # html_theme_options = {}
